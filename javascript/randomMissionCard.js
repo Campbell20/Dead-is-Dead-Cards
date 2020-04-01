262a8fc68afe91de511e753344c6aa8db09d249d
Campbell20 <jcampbell145@alamo.edu>
2020-04-01 01:35:36 -0500

    added randomizer for mission cards

---
+++ javascript/randomMissionCard.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+
+
+    $.getJSON('missionCardsData.json', function (json) {
+        console.log(json);
+        var template
+        template = document.getElementsByTagName("template")[0];
+        var divs = template.content.querySelector('div');
+        var a;
+
+        var i = getRandomInt(0, json.length);
+        console.log(i);
+
+            // //go and create new ids
+            // $(".card-title").attr("id", "mission-title" + i);
+            // $(".card-number").attr("id", "mission-id" + i);
+            // $(".mission-span-story").attr("id", "mission-story" + i);
+            // $(".mission-span-obj").attr("id", "mission-objective" + i);
+            // $(".mission-span-note").attr("id", "mission-note" + i);
+            // $(".mission-span-reward").attr("id", "mission-reward" + i);
+            // $(".card-img-top").attr("id", "mission-img" + i);
+
+            //import all the div elements
+            a = document.importNode(divs, true);
+            //add them to the page
+            document.body.appendChild(a);
+
+            //add in the data to each id
+            document.getElementById("mission-title").innerHTML = json[i].title;
+            document.getElementById("mission-id").innerHTML = i;
+            document.getElementById("mission-story").innerHTML = json[i].story;
+            document.getElementById("mission-objective").innerHTML = json[i].objective;
+            document.getElementById("mission-note").innerHTML = json[i].note;
+            document.getElementById("mission-reward").innerHTML = json[i].reward;
+            document.getElementById("mission-img").src = json[i].image;
+
+    });
+});
